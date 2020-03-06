3c471969c1e7d7a1defe3fee75d2ba1fa5bc9430
7db9a <repo.7db9a@gmail.com>
2020-03-06 15:02:29 -0500

    fix(make): format TMSU version.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 build:
 		@echo "Build TMSU v$(TMSU_VERSION)."
 		./dockerfile.sh $(TMSU_VERSION) >> Dockerfile
-		#docker build -t tmsu:latest .
+		docker build -t tmsu:$(TMSU_VERSION)-buster .
 
 clean:
 		rm Dockerfile 2>/dev/null || true
--- dockerfile.sh
+++ dockerfile.sh
@@ -3,7 +3,6 @@
 #define parameters which are passed in.
 VERSION=$1
 #define the template.
-echo "building version $VERSION"
 cat  << EOM
 FROM golang:1.14.0-buster
 RUN apt-get update && apt-get install -y \\
@@ -13,7 +12,7 @@ RUN go env -w GOPATH=\$HOME/go && \\
     go get -u golang.org/x/crypto/blake2b && \\
     go get -u github.com/mattn/go-sqlite3 && \\
     go get -u github.com/hanwen/go-fuse/fs
-RUN git clone -b $VERSION https://github.com/oniony/TMSU.git /TMSU
+RUN git clone -b v$VERSION https://github.com/oniony/TMSU.git /TMSU
 WORKDIR /TMSU
 RUN make && make install
 WORKDIR /working
